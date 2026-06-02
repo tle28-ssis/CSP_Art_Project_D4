754d2c472bd5f1af97eb8d00cf8103ce17c70c85
Osun-24 <jkang29@ssis.edu.vn>
2026-06-02 08:38:39 +0700

    updated skier

--- my_animation.py
+++ my_animation.py
@@ -36,10 +36,10 @@ def draw_frame(frame_number, elapsed_seconds, width, height):
     sa.draw_text(40, 140, f"Mouse y: {sa.get_mouse_y()}")
 
     # Draw skier
-    new_y = sa.loop_frames(100, 500, 300, frame_number)
-    new_x = sa.loop_frames(100, 800, 300, frame_number)
+    new_y = sa.loop_frames(250, 500, 250, frame_number)
+    new_x = sa.loop_frames(100, 900, 250, frame_number)
     sa.draw_skier(new_x, new_y, 40)
-
+    
 if __name__ == "__main__":
     # Launch the wrapper and tell it to use our draw_frame function
     sa.start(draw_frame)
